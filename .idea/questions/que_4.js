f35d52ffb10465b41950ece31e80b265a37e6b84
seunghyecho <53107641+seunghyecho@users.noreply.github.com>
2023-01-24 17:20:39 +0900

    문제4, 변수의 타입

---
+++ .idea/questions/que_4.js
@@ -0,0 +1,16 @@
+// 다음 변수 a를 typeof(a)로 넣었을 때 출력될 값과의 연결이 알맞지 않은 것은?
+const a = 1;
+typeof a;
+// number
+
+const a1 = 2.22;
+typeof a1;
+// boolean x -> number o
+
+const a2 = "p";
+typeof a2;
+// string
+
+const a3 = [1, 2, 3];
+typeof a3;
+// object
