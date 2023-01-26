4b6a5ff082244cdc083a6536cd8b8dfcfaf0902b
seunghyecho <53107641+seunghyecho@users.noreply.github.com>
2023-01-26 17:57:26 +0900

    Fix conflict

--- .idea/questions/que_5.js
+++ .idea/questions/que_5.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var a = 10;
 var b = 2;
 
@@ -8,10 +7,3 @@ for (var i = 1; i < 5; i += 2) {
 
 console.log(a + b);
 //16
-=======
-// 다음은 자바스크립트 문법 중에서 False로 취급하는 것들 입니다.
-// 앗, False로 취급하지 않는 것이 하나 있네요! True를 찾아주세요.
-
-// false : NaN, "", 0, undefined, null
-// true : 1
->>>>>>> 28f5a85 (문제6, False)
