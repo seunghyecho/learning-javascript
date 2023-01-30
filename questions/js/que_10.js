9890b944c9ef52fb597f86b6aa1518084ad7c11e
seunghyecho <53107641+seunghyecho@users.noreply.github.com>
2023-01-30 16:31:37 +0900

    문제10, 별 찍기

---
+++ questions/js/que_10.js
@@ -0,0 +1,14 @@
+window.onload = function () {
+  for (let i = 0; i < 5; i++) {
+    for (let j = 4; j > i; j--) {
+      document.write("&nbsp");
+    }
+    for (let j = 0; j <= i; j++) {
+      document.write("*");
+    }
+    for (let j = 1; j <= i; j++) {
+      document.write("*");
+    }
+    document.write("<br>");
+  }
+};
