f0b6d96d10dc5b9164104426057a7c2425f2ef3f
seunghyecho <53107641+seunghyecho@users.noreply.github.com>
2023-01-30 16:33:40 +0900

    Add answer

--- questions/js/que_10.js
+++ questions/js/que_10.js
@@ -1,6 +1,6 @@
 window.onload = function () {
-  for (let i = 0; i < 5; i++) {
-    for (let j = 4; j > i; j--) {
+  for (let i = 0; i < 10; i++) {
+    for (let j = 9; j > i; j--) {
       document.write("&nbsp");
     }
     for (let j = 0; j <= i; j++) {
@@ -11,4 +11,21 @@ window.onload = function () {
     }
     document.write("<br>");
   }
+
+  // Answer
+  const n = prompt("숫자를 입력하세요.");
+  let tree = "";
+
+  for (let i = 1; i <= n; i++) {
+    let star = "";
+    for (let j = 1; j <= n - i; j++) {
+      star += " ";
+    }
+    for (let k = 1; k <= 2 * i - 1; k++) {
+      star += "*";
+    }
+    tree += star + "\n";
+  }
+
+  console.log(tree);
 };
