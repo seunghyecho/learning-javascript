5cf686701bad14251932760a7811e93950d54503
seunghyecho <tmdgp523@gmail.com>
2023-02-09 00:51:50 +0900

    문제15, 자기소개
    - template literals

---
+++ questions/js/one/que_15.js
@@ -0,0 +1,6 @@
+window.onload = function () {
+  const name = window.prompt("Write your name");
+  // document.write("HELLO I AM" + name);
+  // es6 expression, Template literals
+  document.write(`HELLO I AM ${name}`);
+};
