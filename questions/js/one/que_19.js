f6efb1f63b57ae7ae94547e1117e8bbef5138e79
seunghyecho <tmdgp523@gmail.com>
2023-02-15 22:04:16 +0900

    문제 19, 제곱승

---
+++ questions/js/one/que_19.js
@@ -0,0 +1,13 @@
+window.onload = function () {
+  const score = prompt("두 숫자").split(",");
+
+  /*
+   * answer
+   * console.log(Math.pow(parseInt(score[0], 10), parseInt(score[1], 10)));
+   */
+
+  for (var i = 0; i < score.length; i++) {
+    const a = parseInt(score[i], 10);
+    return console.log(a ** score[i]);
+  }
+};
