78e0a267ed7d81bf4388f26991f60f532a19bb73
seunghyecho <53107641+seunghyecho@users.noreply.github.com>
2023-04-20 15:49:11 +0900

    주석 추가

--- questions/js/40/41.js
+++ questions/js/40/41.js
@@ -1,33 +1,15 @@
 window.onload = function () {
   const number = prompt("숫자가 주어지면 소수인지 아닌지 판별하는 프로그램");
-  //(소수 : 1과 자기 자신만으로 나누어떨어지는 1보다 큰 양의 정수)
+  // 소수 : 1과 자기 자신만으로 나누어떨어지는 1보다 큰 양의 정수
+  // 다른 말로 하면, 1과 자기 자신이 아닐때 나누어 떨어지면 소수가 아니다.
 
   for (let i = 2; i < number; i++) {
+    console.log(number, i);
     if (number % i === 0) {
-      console.log("소수가 아니다, NO");
+      console.log("소수가 아니다.");
       return false;
     }
   }
 
-  console.log("소수다, YES");
+  console.log("소수다.");
 };
-
-/*
-const num = prompt("숫자를 입력하세요.");
-function check_prime(num) {
-  for (let i=2; i<num; i++) {
-    const result = num % i;
-    if (result === 0) {
-      console.log('NO');
-      return false;
-    }
-  }
-  if (num === 1) {
-    console.log('NO');
-    return;
-  }
-  console.log('YES');
-}
-
-check_prime(num);
-*/
